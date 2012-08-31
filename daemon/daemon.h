7685c27d85865a50fb96962a44e141a15459ed26
Richard W.M. Jones <rjones@redhat.com>
2012-08-31 13:48:18 +0100

    EPEL 5: Revert "daemon: Remove e2prog hack (only needed for RHEL 5)."
    This reverts commit 163e030ee13644ad9efe1e078aff47b879657333.

--- daemon/daemon.h
+++ daemon/daemon.h
@@ -185,6 +185,8 @@ extern int filesystem_available (const char *filesystem);
 extern int sync_disks (void);
 
 /*-- in ext2.c --*/
+extern int e2prog (char *name); /* Massive hack for RHEL 5. */
+
 /* Confirmed this is true up to ext4 from the Linux sources. */
 #define EXT2_LABEL_MAX 16
 
